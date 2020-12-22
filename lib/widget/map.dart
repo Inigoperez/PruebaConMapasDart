57fcacd9d3a256ff45a2a6a71571f10f61b4a9d5
2dam3 <ikbem@plaiaundi.net>
2020-12-22 09:34:07 +0100

    elimino widget antiguo del mapa y añado el nuevo

--- lib/widget/map.dart
+++ lib/widget/map.dart
@@ -1,52 +1,24 @@
-import 'dart:async';
-import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:provider/provider.dart';
-import 'package:prueba_mapa/providers/GPS.dart';
+import "package:flutter_polyline_points/flutter_polyline_points.dart";
+import "package:google_maps_flutter/google_maps_flutter.dart";
+import "package:location/location.dart";
+import "package:flutter/material.dart";
+import "dart:async";
 
-class Mapa extends StatefulWidget {
-  Mapa({Key key}) : super(key: key);
+const double CAMERA_ZOOM = 16;
+const double CAMERA_TILT = 80;
+const double CAMERA_BEARING = 30;
+const LatLng SOURCE_LOCATION = LatLng(42.747932, -71.167889);
+const LatLng DEST_LOCATION = LatLng(37.335685, -122.0605916);
 
+class MapPage extends StatefulWidget {
   @override
-  _Mapa createState() => _Mapa();
+  State<StatefulWidget> createState() => MapPageState();
 }
 
-class _Mapa extends State<Mapa> {
+class MapPageState extends State<MapPage> {
   @override
   Widget build(BuildContext context) {
-    final posicion = Provider.of<GPS>(context);
-
-    print("dentro de widget --line 20--");
-    print(posicion.mi_posicion);
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Mapa"),
-        centerTitle: true,
-      ),
-      body: Stack(
-        children: <Widget>[
-          _widgetMapa(posicion.mi_posicion),
-          FloatingActionButton(
-            onPressed: () => posicion.actualizar(),
-            child: Icon(Icons.navigation),
-            backgroundColor: Colors.green,
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _widgetMapa(CameraPosition position) {
-    Completer<GoogleMapController> _controller = Completer();
-    void _onMapCreated(GoogleMapController controller) {
-      _controller.complete(controller);
-    }
-
-    return GoogleMap(
-      onMapCreated: _onMapCreated,
-      initialCameraPosition: position,
-      mapType: MapType.normal,
-    );
+    // TODO: implement build
+    throw UnimplementedError();
   }
 }
